f328833eb368e512da1ecddd4921ef947fd236b2
Carlos Eduardo <94646666+Carlos1453@users.noreply.github.com>
2023-01-03 17:17:38 -0300

    Button

---
+++ Components/Button/app/src/main/java/com/example/button/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnClick = findViewById(R.id.btnClick);
+
+        btnClick.setOnClickListener(view -> Toast.makeText(this, "Meu botãozao", Toast.LENGTH_SHORT).show());
+
+
+    }
+}
